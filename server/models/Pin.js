ca48674a61beaf5fb95d5bb7bdffe5ca9ce8fa3a
CharmedSatyr <z@charmed.tech>
2017-11-24 08:59:04 +0900

    Renamed Pins model to Pin; added properties

---
+++ server/models/Pin.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const Pin = new Schema({
+  created: {
+    type: Date,
+    required: true,
+    default: new Date()
+  },
+  img: {
+    type: String,
+    default: '../../client/img/image.png' //This is redundant from default client state
+  },
+  likes: [String],
+  title: { type: String, required: true },
+  user: String
+})
+
+export default mongoose.model('Pin', Pin)
