58a3f23682371aae9572dac19882ef18f64acbe0
CharmedSatyr <z@charmed.tech>
2017-11-25 01:30:10 +0900

    +broken image detection

--- client/components/Pin.jsx
+++ client/components/Pin.jsx
@@ -5,16 +5,26 @@
 import React, { Component } from 'react'
 
 //Semantic UI
-import { Header, Image } from 'semantic-ui-react'
+import { Header, Icon, Image } from 'semantic-ui-react'
+
+//App
+import dummy from '../img/image.png'
 
 /*** MAIN ***/
+//Broken Image Fix
+const addDefaultSrc = e => {
+  e.target.src = dummy
+}
+
 const Pin = ({ title, img }) => {
   return (
     <div>
       <Header as="h1" textAlign="center">
         {title}
       </Header>
-      <Image src={img} />
+      <Image src={img} onError={addDefaultSrc} />
+      <Icon name="retweet" />
+      <Icon name="empty heart" />
     </div>
   )
 }
