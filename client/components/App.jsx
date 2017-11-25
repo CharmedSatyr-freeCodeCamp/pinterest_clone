73bef427727d6771663f4a440fcab281be7ce4c9
CharmedSatyr <z@charmed.tech>
2017-11-25 12:43:52 +0900

    Less Confusing Default Username

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -21,7 +21,7 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      loggedUser: 'CharmedSatyr',
+      loggedUser: 'Stranger',
       allPins: []
     }
   }
