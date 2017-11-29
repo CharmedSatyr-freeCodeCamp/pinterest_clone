07ed4154110799234c892b6f03e026339dc40019
CharmedSatyr <z@charmed.tech>
2017-11-29 10:09:33 +0900

    +animations on Like, removed remains of share functions, tweaks

--- client/components/Pin.jsx
+++ client/components/Pin.jsx
@@ -5,10 +5,7 @@
 import React, { Component } from 'react'
 
 //Semantic UI
-import { Header, Icon, Image } from 'semantic-ui-react'
-
-//App
-import dummy from '../img/image.png'
+import { Header, Icon, Image, Transition } from 'semantic-ui-react'
 
 /*** FUNCTIONS ***/
 //Common
@@ -17,42 +14,40 @@ import { f } from '../../common/common.functions.js'
 //Validate image URLs
 import isURL from 'validator/lib/isURL'
 
+/*** RESOURCES ***/
+//Dummy Image
+import dummy from '../img/image.png'
+
 /*** MAIN ***/
 export default class Pin extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      //likes: this.props.likes.length,
-      //loggedUserLike: this.props.loggedUserLike
-      //loggedUserShare: false
+      /*empty and full are used for the Like/Unlike animation - directly  *
+       *checking this.props.loggedUserLike in <Transition/> doesn't work  */
+      empty: !this.props.loggedUserLike,
+      full: this.props.loggedUserLike
     }
     this.addDefaultSrc = this.addDefaultSrc.bind(this)
+    this.deletePin = this.deletePin.bind(this)
     this.toggleLikePin = this.toggleLikePin.bind(this)
-    //this.share = this.share.bind(this)
-    //this.unshare = this.unshare.bind(this)
   }
   //Broken image fix on error
   addDefaultSrc(e) {
     e.target.src = dummy
   }
+  deletePin() {
+    const obj = {
+      title: this.props.title,
+      img: this.props.img,
+      owner: this.props.loggedUser
+    }
+    const data = encodeURIComponent(JSON.stringify(obj))
+    f('DELETE', '/api/deletePin/' + data, response => {
+      //console.log(response)
+    })
+  }
   toggleLikePin() {
-    /*These state changes are *only* required       *
-     *to make the Like button respond more quickly. *
-     *Database information about Likes is passed via*
-     *props and automatically saves/updates. Not    *
-     *using because of a bug - Likes wrongly keep   *
-     *their screen position on add/delete Pin.      */
-    /*if (this.state.loggedUserLike === false) {
-      this.setState({
-        likes: this.state.likes + 1,
-        loggedUserLike: true
-      })
-    } else {
-      this.setState({
-        likes: this.state.likes - 1,
-        loggedUserLike: false
-      })
-    }*/
     const obj = {
       title: this.props.title,
       img: this.props.img,
@@ -64,12 +59,10 @@ export default class Pin extends Component {
       //console.log(response)
     })
   }
-  //share() {}
-  //unshare() {}
   render() {
     let url = isURL(this.props.img)
     return (
-      <span>
+      <div>
         {/* Only show the Remove button to the pin's owner */}
         {this.props.loggedUser === this.props.owner ? (
           <Icon
@@ -78,15 +71,7 @@ export default class Pin extends Component {
             color="red"
             size="large"
             onClick={() => {
-              const obj = {
-                title: this.props.title,
-                img: this.props.img,
-                owner: this.props.loggedUser
-              }
-              const data = encodeURIComponent(JSON.stringify(obj))
-              f('DELETE', '/api/deletePin/' + data, response => {
-                console.log(response)
-              })
+              this.deletePin()
             }}
           />
         ) : null}
@@ -95,35 +80,34 @@ export default class Pin extends Component {
         </Header>
         <Image src={url ? this.props.img : dummy} onError={this.addDefaultSrc} />
 
-        {/* Share button
-        <Icon link name="retweet" size="large" />
-        {this.props.shares.length}
-        */}
         {/* Like button */}
-        {/*this.state.loggedUserLike*/ this.props.loggedUserLike ? (
-          <Icon
-            link
-            name="heart"
-            size="large"
-            color="red"
-            onClick={() => {
-              this.toggleLikePin()
-            }}
-          />
+        {this.props.loggedUserLike ? (
+          <Transition animation={'pulse'} duration={500} visible={this.state.full}>
+            <Icon
+              link
+              name="heart"
+              size="large"
+              color="red"
+              onClick={() => {
+                this.toggleLikePin()
+              }}
+            />
+          </Transition>
         ) : (
-          <Icon
-            link
-            name="empty heart"
-            size="large"
-            onClick={() => {
-              this.toggleLikePin()
-            }}
-          />
+          <Transition animation={'pulse'} duration={500} visible={this.state.empty}>
+            <Icon
+              link
+              name="empty heart"
+              size="large"
+              onClick={() => {
+                this.toggleLikePin()
+              }}
+            />
+          </Transition>
         )}
-        {/*this.state.likes*/}
         {this.props.likes.length}
         <span style={{ float: 'right' }}>{this.props.owner}</span>
-      </span>
+      </div>
     )
   }
 }
