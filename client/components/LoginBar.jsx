4fe65acff7777f85262de8f79a4beb0885455e95
CharmedSatyr <z@charmed.tech>
2017-12-03 22:20:24 +0900

    Improved Login to use a modal popup

--- client/components/LoginBar.jsx
+++ client/components/LoginBar.jsx
@@ -5,11 +5,12 @@
 import React, { Component } from 'react'
 
 //Semantic UI React
-import { Container, Image, Icon, Menu } from 'semantic-ui-react'
+import { Button, Header, Container, Image, Icon, Menu, Modal } from 'semantic-ui-react'
 
 /*** RESOURCES ***/
 //Satyr
 import glowsatyr from '../img/Glowing-Black-Satyr.png'
+import satyr from '../img/Black-Satyr.png'
 
 /*** MAIN ***/
 const LoginBar = () => {
@@ -17,22 +18,48 @@ const LoginBar = () => {
     <Menu fixed="top" inverted>
       <Container>
         <Menu.Item header>
-          {
-            <Image
-              alt="Glowing Black Satyr"
-              size="mini"
-              src={glowsatyr}
-              style={{ marginRight: '1.5em' }}
-            />
-          }
+          <Image
+            alt="Glowing Black Satyr"
+            size="mini"
+            src={glowsatyr}
+            style={{ marginRight: '1.5em' }}
+          />
           Welcome to Sorcerer City, a fantasy-themed Pinterest clone built for freeCodeCamp
         </Menu.Item>
-        <Menu.Item as="a" href="/auth/github" position="right">
-          <Icon fitted name="github" /> GitHub
-        </Menu.Item>
-        <Menu.Item as="a" href="/auth/twitter" position="right">
-          <Icon fitted name="twitter" /> Twitter
-        </Menu.Item>
+        <Modal
+          size="mini"
+          trigger={
+            <Menu.Item as="a" position="right">
+              Login
+            </Menu.Item>
+          }
+        >
+          <Image
+            alt="Satyr logo"
+            centered
+            height="75px"
+            src={satyr}
+            style={{ marginTop: 12, marginBottom: -8 }}
+          />
+          <Modal.Header>
+            <Header textAlign="center">Welcome to Sorcerer City</Header>
+          </Modal.Header>
+          <Modal.Content>
+            <Modal.Description>
+              <Header as="h3" textAlign="center" style={{ marginBottom: -2 }}>
+                Click below to log in.
+              </Header>
+              <br />
+            </Modal.Description>
+            <Button fluid color="grey" href="/auth/github">
+              <Icon name="github" />Continue with GitHub
+            </Button>
+            <br />
+            <Button fluid color="twitter" href="/auth/twitter">
+              <Icon name="twitter" />Continue with Twitter
+            </Button>
+          </Modal.Content>
+        </Modal>
       </Container>
     </Menu>
   )
