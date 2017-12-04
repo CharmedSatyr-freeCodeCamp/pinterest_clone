3b4b9eeae20856b8f42e94a0c4c32295666f1a43
CharmedSatyr <z@charmed.tech>
2017-12-05 05:32:52 +0900

    +comment

--- client/controllers/socket.client.jsx
+++ client/controllers/socket.client.jsx
@@ -5,6 +5,7 @@ import openSocket from 'socket.io-client'
 const clientSocket = openSocket()
 
 /*** MAIN - FUNCTION CALLED IN APP COMPONENT ***/
+//Keeps Pins up to date across users/browsers
 export const start = (interval, cb1, cb2) => {
   clientSocket.on('start', cb1)
   clientSocket.on('updateAllPins', cb2)
