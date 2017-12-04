223cd5482e5646ae5d7cd55b4a92721b37ff3196
CharmedSatyr <z@charmed.tech>
2017-12-04 23:44:18 +0900

    +rel=noopener to footer links

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -12,12 +12,18 @@ const Footer = () => {
   return (
     <Menu fixed="bottom" inverted>
       <Container>
-        <Menu.Item as="a" href="https://github.com/CharmedSatyr/pinterest_clone/" target="_blank">
+        <Menu.Item
+          as="a"
+          href="https://github.com/CharmedSatyr/pinterest_clone/"
+          rel="noopener"
+          target="_blank"
+        >
           Designed and built by &nbsp;<Icon fitted name="github" /> CharmedSatyr
         </Menu.Item>
         <Menu.Item
           as="a"
           href="https://github.com/CharmedSatyr/pinterest_clone/blob/master/LICENSE"
+          rel="noopener"
           target="_blank"
         >
           Code licensed under GPL-3.0
